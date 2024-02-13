a151381b0169971ba50c454c2ee7a869113d2b42
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2024-02-13 12:43:27 -0600

    added picture to about

--- ProPortfolio/src/components/pages/Aboutme.jsx
+++ ProPortfolio/src/components/pages/Aboutme.jsx
@@ -5,14 +5,15 @@ export default function Aboutme() {
       <h1>About Me</h1>
       {/*add a picture of yourself on the feft side of the page using mdb-react-ui-kit*/}
      
-      
+      <div style= {{display: 'flex', alignItems: 'center'}}>
+        <Image 
+        src={ `${Process.env.PUBLIC_URL}/Profile.jpg`} alt= 'Profile' roundedCircle 
+        style={{ width: '150px',  marginRight: '20px'}}
+        />
+
+      </div>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Hi! I am an enthusiastic and motivated junior Full Stack Web Developer with a background in the tech support and education. I am passionate about creating and developing applications that are user-friendly and efficient. I am a quick learner and a team player who is always eager to learn new technologies and languages. I am a graduate of the University of Wisconsin,Full Stack Web Development Bootcamp. I am skilled in HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB, and React.js.  I am excited to bring my skills to a team that values creativity, collaboration, and innovation. I am a native Spanish speaker and I am fluent in English.  I enjoy spending time with my family, traveling, and learning new things.
       </p>
     </div>
   );
